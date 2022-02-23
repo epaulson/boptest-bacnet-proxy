bbe49f92633d1e058351e5e57f285d51202a1a12
Erik Paulson <epaulson@unit1127.com>
2022-02-23 14:42:25 -0600

    initial commit

---
+++ BopTestProxy.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+
+"""
+BOPTEST Proxy Server
+
+This sample application uses BOPTEST framework to create a virtual building
+and make it available over BACnet. To run it, follow the install instructions
+from https://github.com/ibpsa/project1-boptest and run it with testcase1,
+ala 
+TESTCASE=testcase1 docker compose up
+
+Then, in another terminal, run python BopTestProxy.py testcase1.json
+
+Finally, on another machine, use the BACnet client of your choice to
+observe the state of the simulation throough BACnet and optionally 
+overwrite some control point values to influence the simulation
+
+
+This code is based on the OpenWeatherServer.py sample and remains almost
+identical to that sample.
+"""
+
+import os
+import requests
+import time
+import json
+
+from bacpypes.debugging import bacpypes_debugging, ModuleLogger
+from bacpypes.consolelogging import ConfigArgumentParser
+
+from bacpypes.core import run, deferred
+from bacpypes.task import recurring_function
+
+from bacpypes.basetypes import DateTime
+from bacpypes.primitivedata import Real
+from bacpypes.object import AnalogValueObject, DateTimeValueObject
+
+from bacpypes.app import BIPSimpleApplication
+from bacpypes.service.device import DeviceCommunicationControlServices
+from bacpypes.service.object import ReadWritePropertyMultipleServices
+from bacpypes.local.device import LocalDeviceObject
+from bacpypes.local.object import AnalogValueCmdObject, Commandable
+from bacpypes.object import register_object_type, AnalogInputObject
+
+# some debugging
+_debug = 0
+_log = ModuleLogger(globals())
+
+# application interval is refresh time in seconds 
+APPINTERVAL = 5 * 1000 # 5 seconds
+
+# dictionary of names to objects
+objects = {}
+inputs = {}
+nextState = None
+
+
+# TODO - what should some of the BOPTEST objects be - maybe
+# AnalogInputs or BinaryInputs?
+# Not currently using this class
+# TODO - why does BACpyes have a builtin AnalogValueCmdObject but not a builtin AnalogInputCmdObject?
+@bacpypes_debugging
+@register_object_type(vendor_id=999)
+class AnalogInputCmdObject(Commandable(Real), AnalogInputObject):
+    def _set_value(self, value):
+        if _debug:
+            AnalogInputCmdObject._debug("_set_value %r", value)
+
+        # numeric values are easy to set
+        self.presentValue = value
+
+# We are using this class
+# TODO - why are we using this class instead of just AnalogValueCmdObject?
+# This was how OpenWeatherServer.py did it - was it just so it could log the change?
+@bacpypes_debugging
+@register_object_type(vendor_id=999)
+class LocalAnalogValueObject(AnalogValueCmdObject):
+    def _set_value(self, value):
+        if _debug:
+            LocalAnalogValueObject._debug("_set_value %r", value)
+
+        # numeric values are easy to set
+        self.presentValue = value
+
+
+
+# TODO: Rather than a JSON file, look at the BOPTEST /inputs and /measurements
+# endpoints to create BACnet objects for the running testcase. But, we need an instance number
+# and want some control over that, because we want to use that number in the Brick model too
+# TODO: Don't create all of these as the same BACnet object type, some should be
+# AIs or BIs, or maybe AO/BOs
+# TODO: BOPTEST has _activate input points that pair with other settings, but 
+# this proxy could combine those into single BACnet point that if written to 
+# with a higher priority, would override the default BOPTEST setting?
+
+@bacpypes_debugging
+def create_objects(app, configfile):
+    """Create the objects that hold the result values."""
+    if _debug:
+        create_objects._debug("create_objects %r", app)
+    global objects, inputs
+
+    params = None
+    with open(configfile, 'r') as f:
+        boptestpoints = json.load(f)
+
+    for x in ['inputs', 'measurements']:
+        for k,v in boptestpoints[x].items():
+            if _debug:
+                create_objects._debug("    - name: %r", k)
+            units = v['BACpypesUnit']
+            klass = globals()[v['BACpypesType']]
+            instance = v['BACpypesInstanceID']
+            name = k
+
+            obj = klass(objectName = name, objectIdentifier=(klass.objectType, instance), relinquishDefault = 0.0)
+            if _debug:
+                create_objects._debug("    - obj: %r", obj)
+
+            if units is not None:
+                obj.units = units
+        
+            # add it to the application
+            app.add_object(obj)
+
+            # keep track of the object by name
+            objects[k] = obj
+            if x == 'inputs':
+                inputs[k] = obj
+
+
+@recurring_function(APPINTERVAL)
+@bacpypes_debugging
+def update_boptest_data():
+    """Read the current simulation data from the API and set the object values."""
+    if _debug:
+        update_boptest_data._debug("update_boptest_data")
+    global objects, inputs
+
+    # ask the web service
+    # We get results direct from /advance now but you could ask the simulation for historic data
+    #response = requests.put(
+    #    "http://localhost:5000/results", data={'point_name':'TRooAir_y', 'start_time': timestep * 30, 'final_time': (timestep+1)*30}
+    #)
+
+    # TODO: Is there a way to get the current priorty value a point is currently written at?
+    #       that way, we don't have to pass in the overwriteable signals if they match the BOPTest
+    #       presets. This is slightly complicated because BOPTest is "sticky" - once you overwrite the
+    #       the built-in it remembers that setting for future calls to /advance. Work around that here by just
+    #       always overwriting the inputs from whatever is in the BACnet registers
+    # TODO: should some of these signals combine into one BACnet object, e.g. in testcase1, if someone overwrites
+    #       oveAct_u, should the proxy automatically turn on oveAct_activate, without exposing oveAct_activate as
+    #       a BACnet point? We'd have to pair them somehow...
+    signals = {}
+    for k,v in inputs.items():
+        signals[k] = v._highest_priority_value()
+
+    response = requests.post(
+    #    "http://localhost:5000/advance", data={"oveAct_u": next_oveAct_u, "oveAct_activate": next_oveAct_activate}
+        "http://localhost:5000/advance", data=signals
+    )
+    if response.status_code != 200:
+        print("Error response: %r" % (response.status_code,))
+        return
+
+    # turn the response string into a JSON object
+    json_response = response.json()
+
+    # set the object values
+    # We advance the simulation by 5 seconds at each call to the loop, but we don't update the external world
+    # with those results until the NEXT call to this function. 
+    #
+    # TODO: rather than using recurring_function, we should schedule ourselves to be called again at (5secs-elapsed_function_time)
+    # because if say the call to /advance takes 3 seconds, we want to get called again in 2 seconds, not in 5 seconds. 
+    global nextState
+    if nextState:
+      for k, v in nextState.items():
+          if _debug:
+              update_boptest_data._debug("    - k, v: %r, %r", k, v)
+
+          if k in objects:
+              objects[k]._set_value(v)
+
+    nextState = json_response
+
+# BAC0 uses the ReadPropertyMultiple service so make that available
+@bacpypes_debugging 
+class ReadPropertyMultipleApplication(
+   BIPSimpleApplication,
+   ReadWritePropertyMultipleServices,
+   DeviceCommunicationControlServices,
+   ):
+   pass
+
+@bacpypes_debugging
+def main():
+    global vendor_id
+
+    parser = ConfigArgumentParser(description=__doc__)
+
+    parser.add_argument('boptest_config', type=str, help='JSON file with mappings from BOPTest to BACnet')
+    # parse the command line arguments
+    args = parser.parse_args()
+    
+
+    if _debug:
+        _log.debug("initialization")
+    if _debug:
+        _log.debug("    - args: %r", args)
+
+    # TODO: Take the URL as a commandline argument
+    # TODO: check the results to make sure we acutally get an OK!
+    # TODO: Take a commandline option for warmup period - e.g. let the simulation settle in respoonse to
+    # outside air temps etc before starting
+    global nextState
+    url = "http://localhost:5000" 
+    res = requests.put('{0}/initialize'.format(url), data={'start_time':0, 'warmup_period':0} ).json()
+    nextState = res
+
+    # We advance the simulation by 5 seconds at each call to /advance, and APPINTERVAL is also 5 seconds, so the simulationo
+    # moves in sync with wallclock time. To see things happen faster, set this time greater than 5 seconds
+    res = requests.put('{0}/step'.format(url), data={'step':5})
+
+    # make a device object
+    this_device = LocalDeviceObject(ini=args.ini)
+    if _debug:
+        _log.debug("    - this_device: %r", this_device)
+
+    # make a sample application
+    this_application = ReadPropertyMultipleApplication(this_device, args.ini.address)
+
+    # create the objects and add them to the application
+    create_objects(this_application, args.boptest_config)
+    
+    # run this update when the stack is ready
+    deferred(update_boptest_data)
+
+    if _debug:
+        _log.debug("running")
+
+    run()
+
+    if _debug:
+        _log.debug("fini")
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# boptest-bacnet-proxy
-A BACnet proxy to the building simulations in the BOPTEST framework. Uses BACpypes and Python
+# BOPTEST-BACnet-proxy
+A BACnet proxy to the building simulations in the [BOPTEST](https://github.com/ibpsa/project1-boptest) framework. Uses [BACpypes](https://github.com/JoelBender/bacpypes) and Python.
+
+## Overview
+The BOPTEST (Building Optimization Performance Testing) framework is a project of the International Building Peformance Simulation Association (IBPSA), the US Department of Energy, several US National Labs, and other collaborators. BOPTEST was created to benchmark building control algorithms, and to make it easier to get started.
+You don't need to know the in-depth details of building simulation in order to use BOPETST.
+
+BOPTEST builds on [Modelica](https://en.wikipedia.org/wiki/Modelica) and the [Modelica Buildings Library](https://simulationresearch.lbl.gov/modelica/) to run high-fidelity physics based building simulations, along with some custom Modelica blocks that use the FMI standard to exchange data between the simulation and the outside world. 
+BOPTEST packages all of the software up along with preconfigured building models into self-contained Docker images. The Docker images contain a REST endpoint built on Flask that can drive the simulation and exchange data between the caller and the simulation, including overriding some control signals and setpoints the simulation is using. 
+The Docker image has everything necessary for the simulation - it includes realistic, granular weather and occupancy data, layout information about the zones and thermal properties of the building, and baseline controls for the building. 
+You can literally make a call to the `/initialize` endpoint, and then call the `/advance` endpoint 120\*24\*365 times, each time telling the `/advance` endpoint to advance the state of the simulation by 30 seconds, you'll get at the end a simulated history of 1 years' worth of building performance, along with a set of key performance indicaors (KPIs) calculated for the building on how it did for energy usage/occupant comfort/etc, using the baseline control configuration in the system. 
+Happily, it's pretty fast, so you can get a year's worth of simulated building data in only a few minutes/hours of wallclock time.
+
+Of course, the point of BOPTEST is to be able to test different controllers and control algorithms on realistic building configurations, so you can override the control signals and setpoints of the baseline controllers at each call to the `/advance` end point to test different algorithms. 
+The BOPTEST distribution includes several different preconfigured simulatable buildings, each one called a 'test case'. 
+
+But, just because you can run the simulation faster than wallclock time doesn't mean you have to, and that's what we've been experimenting with. 
+This repository has a simple server - the `BoptestBACnetProxy` - that drives a BOPTEST simulation at wallclock speeds. 
+Every 5 seconds of realworld time, the `BoptestBACnetProxy` advances the BOPTEST simulation forward by 5 seconds. 
+To interact with the simulation, the `BoptestBACnetProxy` reads the current state of the simulation out of BOPTEST and makes those values available as BACnet objects. 
+The `BoptestBACnetProxy` can also be written to using BACnet, and any of the "input" points that are overwritten in the proxy and passed in to the BOPTEST simulation.
+This way, users can interact with the building simulation through the BACnet client or controller of their choice and override setpoints and such, and see physically-correct results in the virtual sensor objects also through BACnet.
+
+The end result is a fully digital building that can be created and interacted with as though it was a real building, using standard protocols.
+
+
+## Getting Started
+To run it, follow the install instructions
+from https://github.com/ibpsa/project1-boptest and run it with testcase1,
+```
+TESTCASE=testcase1 docker compose up
+```
+
+Then, in another terminal, edit the `BACpypes.ini` file to match the IP address details of your site, and run 
+```
+python BopTestProxy.py testcase1.json
+```
+(remember that ObjectIdentifer property in BACnet must be unique across your entire BACnet installation, so you may need to change the 599 to something else, consult your local BACnet administrator)
+
+Finally, on another machine, use the BACnet client of your choice to observe the state of the simulation throough BACnet and optionally overwrite some control point values to influence the simulation.
+Remember that BACnet uses UDP and port 47808 ('BAC0' in hex) so you may need to adjust your firewall rules on both machines.
+
+## Configuration
+At the moment, we are creating JSON files to do the mapping between BACnet and BOPTEST, one for each test case, though they are very simple. In the future, we may drop the JSON files and get the mapping information right from BOPTEST using the `/inputs` and `/measurements` endpoints. 
+The reason we do not currently do so is to be sure we have stable BACnet object instance IDs.
+
+## Brick Models
+We are working on creating a [Brick model](https://brickschema.org/) for each of the test cases.
+The Brick models include the BACnet references necessary for Brick+BACnet-enabled software to interact with the BOPTEST simulation, through the proxy.
+
+## License
+The `BopTestProxy.py` is a slightly modified version of the OpenWeatherServer.py sample from the BACpypes package, which was written by Joel Bender and was made available under the MIT License. 
+
